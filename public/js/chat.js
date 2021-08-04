e315d2c2f47a48500d675e34cb84b4ef9e72a8f2
menaaziz27 <menaaziz27@gmail.com>
2021-08-04 12:06:10 +0200

    event acknowledgement

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,5 +7,29 @@ socket.on('message', newMessage => {
 document.querySelector('#form-message').addEventListener('submit', e => {
 	e.preventDefault();
 	const message = document.querySelector('#message').value;
-	socket.emit('newMessage', message);
+	socket.emit('sendMessage', message, error => {
+		if (error) {
+			return console.log(error);
+		}
+		console.log('message is delivered!');
+	});
 });
+
+function sendLocation() {
+	if (!navigator.geolocation) {
+		return alert('geolocation not supported!');
+	}
+
+	navigator.geolocation.getCurrentPosition(position => {
+		console.log(position);
+		const long = position.coords.longitude;
+		const lat = position.coords.latitude;
+		socket.emit('send-location', { long, lat }, () => {
+			console.log('location shared!');
+		});
+	});
+}
+
+document
+	.querySelector('#send-location')
+	.addEventListener('click', sendLocation);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const PORT = 8080;
 const app = express();
@@ -16,12 +17,29 @@ app.get('/', (req, res, next) => {
 });
 
 io.on('connection', socket => {
-	console.log('welcome a new connection');
+	console.log('New WebSocket connection');
 
-	socket.emit('message', 'hello client!');
+	socket.emit('message', 'Welcome!');
+	socket.broadcast.emit('message', 'A new user has joined!');
 
-	socket.on('newMessage', newMessage => {
-		io.emit('message', newMessage);
+	socket.on('sendMessage', (message, callback) => {
+		const filter = new Filter();
+		if (filter.isProfane(message)) {
+			return callback('Profanity is not allowed');
+		}
+		io.emit('message', message);
+		callback();
+	});
+
+	socket.on('send-location', (coords, callback) => {
+		io.emit(
+			'message',
+			`https://google.com/maps?q=${coords.lat},${coords.long}`
+		);
+		callback();
+	});
+	socket.on('disconnect', () => {
+		io.emit('message', 'A user has left!');
 	});
 });
 
