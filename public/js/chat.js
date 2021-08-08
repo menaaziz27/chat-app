f0ed7c1a55f07112e40854061a7b4984b8935ffc
menaaziz27 <menaaziz27@gmail.com>
2021-08-08 15:43:58 +0200

    scroll to bottom

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,6 +16,28 @@ const { username, room } = Qs.parse(location.search, {
 	ignoreQueryPrefix: true,
 });
 
+const autoscroll = () => {
+	const $newMessage = $messages.lastElementChild;
+
+	// height of the new message
+	const newMessageStyles = getComputedStyle($newMessage);
+	const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+	const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+	// visible height
+	const visibleHeight = $messages.offsetHeight;
+
+	// height of messages container
+	const containerHeight = $messages.scrollHeight;
+
+	// how far have i scrolled ?
+	const scrollOffset = $messages.scrollTop + visibleHeight;
+
+	if (containerHeight - newMessageHeight <= scrollOffset) {
+		$messages.scrollTop = $messages.scrollHeight;
+	}
+};
+
 socket.on('message', message => {
 	console.log(message);
 	const html = Mustache.render($messageTemplate, {
@@ -24,6 +46,7 @@ socket.on('message', message => {
 		createdAt: moment(message.createdAt).format('h:mm a'),
 	});
 	$messages.insertAdjacentHTML('beforeend', html);
+	autoscroll();
 });
 
 socket.on('roomData', ({ users, room }) => {
@@ -42,6 +65,7 @@ socket.on('share-location', message => {
 		createdAt: moment(message.createdAt).format('h:mm a'),
 	});
 	$messages.insertAdjacentHTML('beforeend', html);
+	autoscroll();
 });
 
 function onSubmitForm(e) {
